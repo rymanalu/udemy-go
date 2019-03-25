c47b0becc56b09eb709402fa861398e636125a3e
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-03-25 16:52:52 +0700

    028

---
+++ 028/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println("A:")
+	fmt.Println(x, y, z)
+
+	fmt.Println()
+
+	fmt.Println("B:")
+	fmt.Println("x =", x)
+	fmt.Println("y =", y)
+	fmt.Println("z =", z)
+}
