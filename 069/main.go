8f89ef46d5b59f183ad1ef6d91d285d0db72c8cc
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-04-19 15:27:31 +0700

    069

---
+++ 069/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	favSport := "Football"
+
+	switch favSport {
+	case "Badminton":
+		fmt.Println("Badminton")
+	case "Pingpong":
+		fmt.Println("Pingpong")
+	case "Football":
+		fmt.Println("Football")
+	case "Swimming":
+		fmt.Println("Swimming")
+	}
+}
