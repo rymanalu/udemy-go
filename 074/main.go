eaefdfbaf7aec37e0a4dc4a242469476d2b58d03
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-04-21 15:50:33 +0700

    074

---
+++ 074/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{0, 1, 1, 2, 3, 5}
+	fmt.Println(x)
+	fmt.Println(x[1:])
+	fmt.Println(x[1:3])
+
+	for i, v := range x {
+		fmt.Println(i, v)
+	}
+
+	for i := 0; i < len(x); i++ {
+		fmt.Println(i, x[i])
+	}
+}
