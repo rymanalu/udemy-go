7dea97e122a802fe595ee722ba169cc61de004bc
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-04-22 19:22:51 +0700

    075

---
+++ 075/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{0, 1, 1, 2, 3, 5}
+	fmt.Println(x)
+	x = append(x, 8, 13, 21, 35, 56)
+	fmt.Println(x)
+
+	y := []int{234, 456, 678, 987}
+	x = append(x, y...)
+	fmt.Println(x)
+}
