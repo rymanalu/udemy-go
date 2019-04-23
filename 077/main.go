d99ff4ad6e7ff3e9300eb5a8308d882201560453
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-04-23 18:18:51 +0700

    077

---
+++ 077/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := make([]int, 10, 12)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	x[0] = 42
+	x[9] = 69
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	x = append(x, 666)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	x = append(x, 999)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	x = append(x, 1999)
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+}
