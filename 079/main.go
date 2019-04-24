e55ae4548800384d6f5dae1025197e65f6af87b2
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-04-24 21:36:28 +0700

    079

---
+++ 079/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string]int{
+		"Roni":   24,
+		"Kesbor": 69,
+	}
+	fmt.Println(m)
+
+	fmt.Println(m["Roni"])
+
+	fmt.Println(m["Random"])
+
+	v, ok := m["Random"]
+	fmt.Println(v)
+	fmt.Println(ok)
+
+	if v, ok := m["Roni"]; ok {
+		fmt.Println("This is the if print", v)
+	}
+}
