76ccf7414d6414d8946037a3b95e7f052fce5675
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-04-25 23:18:34 +0700

    089

---
+++ 089/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		"bond_james":      []string{"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
+		"no_dr":           []string{"Being evil", "Ice cream", "Sunsets"},
+	}
+
+	for k1, v1 := range m {
+		fmt.Println("Record for:", k1)
+
+		for i, v2 := range v1 {
+			fmt.Printf("\tindex: %v\tvalue: %v\n", i, v2)
+		}
+	}
+}
