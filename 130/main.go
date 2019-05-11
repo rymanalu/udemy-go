fe38a940774c3fa748fc6a3067202f41913b1864
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-05-11 11:59:30 +0700

    130

---
+++ 130/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	p1 := person{
+		FirstName: "John",
+		LastName:  "Lennon",
+		Age:       40,
+	}
+	fmt.Println(p1)
+
+	p2 := person{
+		FirstName: "Paul",
+		LastName:  "McCartney",
+		Age:       76,
+	}
+	fmt.Println(p2)
+
+	people := []person{p1, p2}
+	fmt.Println(people)
+
+	bs, err := json.Marshal(people)
+
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+
+	fmt.Println(string(bs))
+}
