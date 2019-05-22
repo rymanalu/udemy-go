4ee23512d0e5143587d88674e83c96ee32950ce4
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-05-22 13:30:50 +0700

    159

---
+++ 159/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	even := make(chan int)
+	odd := make(chan int)
+	quit := make(chan int)
+
+	go send(even, odd, quit)
+
+	receive(even, odd, quit)
+
+	fmt.Println("exit")
+}
+
+func send(e, o, q chan<- int) {
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			e <- i
+		} else {
+			o <- i
+		}
+	}
+	q <- 0
+}
+
+func receive(e, o, q <-chan int) {
+	for {
+		select {
+		case v := <-e:
+			fmt.Println("from the even channel:", v)
+		case v := <-o:
+			fmt.Println("from the odd channel:", v)
+		case v := <-q:
+			fmt.Println("from the quit channel:", v)
+			return
+		}
+	}
+}
