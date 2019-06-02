209fc465e158c88656da6cf7b6129a63e3821cea
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-06-02 16:34:01 +0700

    177

---
+++ 177/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p := person{
+		First: "John",
+		Last:  "Lennon",
+		Sayings: []string{
+			"I'm an Elvis fan because it was Elvis who really got me out of Liverpool.",
+			"You're born in pain and pain is what we're in most of the time. And I think that the bigger the pain, the more gods we need.",
+			"Paul & I enjoyed writing the music for the film, but there were times when we honestly thought we'd never get time to write all the material. We managed to get a couple finished while we were in Paris, and 3 more completed soaking up sun on Miami",
+		},
+	}
+
+	bs, err := toJSON(p)
+
+	if err != nil {
+		log.Fatalln(err)
+
+		return
+	}
+
+	fmt.Println(string(bs))
+}
+
+func toJSON(o interface{}) ([]byte, error) {
+	bs, err := json.Marshal(o)
+
+	if err != nil {
+		err = fmt.Errorf("toJSON error occurred: %v", err)
+		return []byte{}, err
+	}
+
+	return bs, nil
+}
